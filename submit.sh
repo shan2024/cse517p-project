724f86b1767d9e13f4eaed4ee83693e0fcc293c6
Kun Qin <kuqin@microsoft.com>
2025-04-19 23:58:54 -0700

    add our names

--- submit.sh
+++ submit.sh
@@ -6,7 +6,7 @@ rm -rf submit submit.zip
 mkdir -p submit
 
 # submit team.txt
-printf "Victor Zhong,vzhong\nNoah Smith,nasmith\nSanta Claus,sclaus" > submit/team.txt
+printf "Kun Qin,kuqin\nJames Kim,jamesk97" > submit/team.txt
 
 # train model
 python src/myprogram.py train --work_dir work
