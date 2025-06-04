b3bc8d2f6a86d9a092b90740076d843c1884358a
Amelia Payne <ameliappayne@gmail.com>
2025-06-03 22:10:41 -0700

    Accdentaly deleted spanish. Put back

--- src/Transformer_Based/transformer_wrapper.py
+++ src/Transformer_Based/transformer_wrapper.py
@@ -201,3 +201,85 @@ class TransformerModelWrapper:
         avg_dev_loss = total_dev_loss / len(dataloader)
 
         return avg_dev_loss
+    
+    def test_train(self, data_directory, model, continue_training: bool = True, dataset_fraction: float = 1.0, num_epochs: int = 3, lr: float = 1e-4, batch_size=1048):
+
+        dataset = CharDatasetWrapper(self.device, data_directory, self.context_length, dataset_fraction)
+        
+        with open(self.vocab_file_path, "w", encoding="utf-8") as f:
+            json.dump(dataset.vocab(), f, ensure_ascii=False, indent=2)
+
+        # Ensure checkpoints directory exists
+        os.makedirs(os.path.join(self.work_directory, "checkpoints"), exist_ok=True)
+
+        # Prepare datasets
+        num_workers = min(20, multiprocessing.cpu_count() - 2)
+        train_loader = DataLoader(dataset.train_dataset(), batch_size=batch_size, shuffle=True, pin_memory=True, num_workers=num_workers,  prefetch_factor=2, persistent_workers=True)
+        dev_loader = DataLoader(dataset.dev_dataset(), batch_size=batch_size, shuffle=True, pin_memory=True, num_workers=num_workers,  prefetch_factor=2, persistent_workers=True)
+
+        original_model = None
+        
+        self.model = model
+            
+        original_model = self.model
+            
+        if hasattr(torch, 'compile'):
+            self.model = torch.compile(self.model)
+        
+        self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=lr)
+        self.loss_fn = torch.nn.CrossEntropyLoss()
+        
+        scaler = GradScaler()
+
+        self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=lr)
+        self.loss_fn = torch.nn.CrossEntropyLoss()
+        scheduler = OneCycleLR(
+            self.optimizer,
+            max_lr=lr,
+            steps_per_epoch=len(train_loader),
+            epochs=num_epochs,
+            pct_start=0.1,
+            anneal_strategy="cos",
+            final_div_factor=1e4,
+        )
+
+        for epoch in range(num_epochs):
+            self.model.train()
+            total_train_loss = 0
+
+            # Add progress bar
+            for x_batch, y_batch in tqdm(train_loader, desc=f"Epoch {epoch+1}/{num_epochs}"):
+                x_batch = x_batch.to(self.device)
+                y_batch = y_batch.to(self.device)
+                self.optimizer.zero_grad()
+
+                # AMP forward
+                with autocast(device_type=self.device.type):
+                    logits = self.model(x_batch)
+                    loss = self.loss_fn(logits, y_batch)
+
+                # AMP backward
+                scaler.scale(loss).backward()
+                torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=1.0)
+                scaler.step(self.optimizer)
+                scaler.update()
+                scheduler.step()
+
+                scaler.unscale_(self.optimizer)
+
+                total_train_loss += loss.item()
+                
+            avg_train_loss = total_train_loss / len(train_loader)
+
+            dev_loss = self.eval_loss(dev_loader)
+
+            print(f"[train] Epoch {epoch + 1}/{num_epochs} - Train Loss: {avg_train_loss:.4f}. Dev Loss: {dev_loss:.4f}")
+
+            torch.save(self.model.state_dict(), f"{self.model_checkpoint_path}.{epoch}")
+
+        if hasattr(torch, 'compile'):
+            # Need to save the Og model not the compiled version to avoid problems
+            torch.save(original_model.state_dict(), self.model_file_path)
+        else:
+            torch.save(self.model.state_dict(), self.model_file_path)
+        print(f"[train] Model saved to {self.model_file_path}")
